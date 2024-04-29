c667f08d8320a3b48bed7d9e135fb6f4fbff7b8f
Patricio Pilco <ppilco@galavail.com>
2024-04-28 21:17:56 -0500

    Actualizar dependencias para agregar soporte de pago en pedidos

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -3,19 +3,25 @@ import {
   NotImplementedException,
   ParseUUIDPipe,
 } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
 import { ChangeOrderStatusDto } from './dto/change-order-status.dto';
+import { PaidOrderDto } from './dto';
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @MessagePattern('createOrder')
-  create(@Payload() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+  async create(@Payload() createOrderDto: CreateOrderDto) {
+    const order = await this.ordersService.create(createOrderDto);
+    const paymentSession = await this.ordersService.createPaymentSession(order);
+    return {
+      order,
+      paymentSession,
+    }
   }
 
   @MessagePattern('findAllOrders')
@@ -31,5 +37,10 @@ export class OrdersController {
   @MessagePattern('changeOrderStatus')
   changeOrdersStatus(@Payload() changeOrdersStatus: ChangeOrderStatusDto) {
     return this.ordersService.changeStatus(changeOrdersStatus);
+  }
+
+@EventPattern('payment.succeeded')
+  paidOrder(@Payload() paidOrderDto:PaidOrderDto){
+    return this.ordersService.paidOrder(paidOrderDto);
   }
 }
